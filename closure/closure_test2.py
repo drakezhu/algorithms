80802aa1feb7bafac2196eab06fef55a13067cfe
yunke zhu <drake@yunkes-MacBook-Pro.local>
2019-10-28 23:45:56 -0400

    another example of python closure

---
+++ closure/closure_test2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+def outerFunction(text): 
+    text = text 
+  
+    def innerFunction(): 
+        print(text) 
+    return innerFunction
+
+if __name__ == '__main__': 
+    myFunction = outerFunction('Hey!')
+    myFunction()
