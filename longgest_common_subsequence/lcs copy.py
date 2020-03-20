154e2b2332dd1a63547be20c20ce1a0e5b6a95bf
yunke zhu <yunke.zhu@tufts.edu>
2020-03-20 17:40:44 -0400

    lcs with string output

---
+++ longgest_common_subsequence/lcs copy.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    
+        def shortestCommonSupersequence(self, str1, str2):
+            i = j = 0
+                m = len(str1)
+                n = len(str2)
+                l = [[0 for i in range(n+1)] for j in range(m+1)] 
+                
+                def LCSLength(X,Y,m,n):
+                    for i in range(1,m+1):
+                        for j in range(1,n+1):
+                            if X[i-1] == Y[j-1]:
+                                l[i][j] = l[i-1][j-1]+1
+                            else:
+                                l[i][j] = max(l[i-1][j],l[i][j-1])
+                
+                def LCS(X, Y, m, n):
+                    if m == 0 or n == 0:
+                        return ""
+                    if X[m-1] == Y[n-1]:
+                        return LCS(X,Y,m-1,n-1) + X[m-1]
+                    if l[m-1][n] > l[m][n-1]:
+                        return LCS(X,Y,m-1,n)
+                    else:
+                        return LCS(X,Y,m,n-1)
+                
+                LCSLength(str1,str2,m,n)
+
+                lcs = LCS(str1,str2,m,n)
+                print x
