ce59c9c95e74e1a0248cebbd81271291083adbbb
yunke zhu <yunke.zhu@tufts.edu>
2020-03-20 18:07:24 -0400

    cool method from lee

--- longgest_common_subsequence/Lattest LCS.py
+++ longgest_common_subsequence/Lattest LCS.py
@@ -28,3 +28,19 @@ class Solution(object):
 
                 lcs = LCS(str1,str2,m,n)
                 print x
+                
+                
+                
+'''
+lee's method
+'''
+def lcs(A, B):
+    n, m = len(A), len(B)
+    dp = [["" for _ in xrange(m + 1)] for _ in range(n + 1)]
+    for i in range(n):
+        for j in range(m):
+            if A[i] == B[j]:
+                dp[i + 1][j + 1] = dp[i][j] + A[i]
+            else:
+                dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1], key=len)
+    return dp[-1][-1]
